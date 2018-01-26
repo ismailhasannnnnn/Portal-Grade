1029f3e2fdcc9128672ad35752384b2e8acf8140
Ismail Hasan <ismailhasan0132@gmail.com>
2018-01-26 00:12:29 -0800

    Able to grab table data for period 1 class, fully functional from logging in to getting data.

--- Portal/main.py
+++ Portal/main.py
@@ -2,11 +2,13 @@ import selenium
 from selenium import webdriver
 import os
 import time
+import csv
 from selenium.webdriver.common.keys import Keys
+from tabula import read_pdf
 
 
-id = raw_input('Please input your ID number: ')
-loginPass = raw_input("And your password: ")
+id = input('Please input your ID number: ')
+loginPass = input("And your password: ")
 clear = lambda: os.system('cls')
 os.system("cls")
 print("Please wait....")
@@ -32,4 +34,21 @@ td.click()
 
 time.sleep(2)
 
-period1 = browser.find_element_by_id('tblassign_1')
+period1 = browser.find_element_by_xpath("//*[@id='tblassign_1']/tbody")
+# for tr in period1.find_elements_by_tag_name('tr'):
+#     with open("output.csv", "wb") as csvFile:
+#         w = csv.writer(csvFile, dialect='excel')
+#         w
+periodOneElements = []
+
+for tr in period1.find_elements_by_tag_name('tr'):
+    periodOneElements.append(tr.text)
+
+
+with open("output.csv", 'w') as resultFile:
+    writer = csv.writer(resultFile, dialect='excel', delimiter=' ')
+    writer.writerows(periodOneElements)
+
+
+
+
